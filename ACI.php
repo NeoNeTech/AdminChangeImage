d4c9a40dc33598deeabe843f3e843d7b4b90a7e0
NeoNeTech <quentin.lloveras@gmail.com>
2019-04-30 15:05:39 +0200

    V 0.2.5
    0.2.5 : Preview Mode
    
          Prévisualisation du logo
          Prévisualisation de l'arrière plan
          Ajout de l'onglet Preview qui permet d'avoir un aperçu de la page de login sans ce déconnecté

--- ACI.php
+++ ACI.php
@@ -4,7 +4,7 @@ Plugin Name: Admin Change Image
 Plugin URI: https://nereus-water.com
 Description: Plugin réaliser pour NEREUS Water, afin de pouvoir changer le logo dans l'interface de connexion
 Author: Quentin LLOVERAS
-Version: 0.2.1
+Version: 0.2.5
 Author URI: https://github.com/NeoNeTech
 */
 define("ACIFOLDER", plugin_dir_url( __FILE__ ));
@@ -33,28 +33,29 @@ function aci_settings(){
   <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
   <h1>Admin Change Image Settings</h1>
   <h2 class="nav-tab-wrapper">
-        <a href="#" id="option-click" class="nav-tab"><?php _e( 'Options', 'textdomain' ); ?></a>
-        <a href="#" id="info-click" class="nav-tab"><?php _e( 'À propos', 'textdomain' ); ?></a>
+    <a href="#options" id="option-click" class="nav-tab"><?php _e( 'Options', 'textdomain' ); ?></a>
+    <a href="#preview" id="preview-click" class="nav-tab"><?php _e( 'Preview', 'textdomain' ); ?></a>
+    <a href="#informations" id="info-click" class="nav-tab"><?php _e( 'À propos', 'textdomain' ); ?></a>
   </h2>
 
-  <div id="options" class="tabs">
-  <h2>Logo :</h2>
+  <div id="options" class="tabs container">
   <form method="POST" action=""/>
+  <h2>Logo :</h2>
   <label for="url-lien">Souhaitez-vous pointez vers votre page d'accueil ?</label>
   <input type="radio" name="url-lien" value="Check" <?php if($settings->lienurl == "Check"){ echo "checked=\"yes\"";} ?>/>Oui
   <input type="radio" name="url-lien" value="Uncheck" <?php if($settings->lienurl == "Uncheck"){ echo "checked=\"yes\"";} ?>/>Non
   <br />
   <label for="alt-logo">Alt du logo :</label>
-  <input type="text" name="alt-logo" class="inputText" value="<?php echo $settings->logoalt; ?>"/>
+  <input type="text" name="alt-logo" class="inputText" value="<?php echo $settings->logoalt; ?>" id="text-alt"/>
   <br />
   <label for="url-logo">URL du logo :</label>
-  <input type="text" name="url-logo" class="inputText" value="<?php echo $settings->logourl; ?>"/>
+  <input type="text" name="url-logo" class="inputText" value="<?php echo $settings->logourl; ?>" id="text-url"/>
   <br />
   <label for="size-logo">Taille du logo :</label>
   <input type="number" name="size-logo" class="inputNum" value="<?php echo $settings->logosize; ?>"/>
   <h2>Arrière plan :</h2>
   <label for="url-image">URL de l'arrière plan :</label>
-  <input type="text" name="url-image" class="inputText" value="<?php echo $settings->imageurl; ?>"/>
+  <input type="text" name="url-image" class="inputText" value="<?php echo $settings->imageurl; ?> " id="bg-url"/>
   <br />
   <label for="size-image">Taille de l'arrière plan :</label>
   <select name="size-image" class="inputNum" size="1">
@@ -77,27 +78,106 @@ function aci_settings(){
   <br/>
   <h2>Texte :</h2>
   <label for="color-text">Couleur :</label>
-  <input type="text" value="<?php echo $settings->colortext; ?>" name="color-text" class="my-color-field" data-default-color="<?php echo $settings->colortext; ?>" />  <br/>
+  <input type="text" vsalue="<?php echo $settings->colortext; ?>" name="color-text" class="my-color-field" data-default-color="<?php echo $settings->colortext; ?>" />  <br/>
   <?php submit_button(); ?>
 </form>
+<div class="previewBanner">
+  <h3>Preview du logo :</h3>
+  <img src="#" id="imgOutput" alt="Merci de rentrer une URL valide" height="250px"/>
+  <h3>Preview de l'arrière plan :</h3>
+  <img src="#" id="bgOutput" alt="Merci de rentrer une URL valide" height="250px"/>
+</div>
+</div>
+
+<div id="preview">
+  <h2>Preview de la page login :</h2>
+  <div class="previewContainer">
+    <img src="<?php echo $settings->logourl; ?>" alt="<?php echo $settings->logoalt; ?>" title="<?php echo $settings->logoalt; ?>" class="logo"/>
+    <div class="form">
+    <form name="loginform" id="loginform" action="#" method="post">
+	<p>
+		<label for="user_login" style="margin-left: 30px;">Identifiant ou adresse e-mail<br>
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" style="margin-left: 30px;width:80%;"></label>
+	</p>
+	<p>
+		<label for="user_pass" style="margin-left: 30px;">Mot de passe<br>
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" style="margin-left: 30px;width:80%;"></label>
+	</p>
+  <div style="display:flex;align-items:center;margin-left: 35px;">
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> Se souvenir de moi</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Se connecter" style="margin-left: 35%;">
+	</p>
+</div>
+	</form>
+</div>
+  <style>
+    .logo {
+      height:75px;
+      margin: 5% auto 5% 43%;
+      text-align: center;
+      position: absolute;
+      transform: scale(<?php echo $settings->logosize; ?>);
+    }
+    #user_login, #user_pass {
+      font-size: 24px;
+      width: 100%;
+      padding: 3px;
+      margin: 2px 6px 16px 0;
+      background: #fbfbfb;
+    }
+
+    #loginform {
+      height: 285px;
+      width: 350px;
+      font-weight: 400;
+      overflow: hidden;
+      background: #fff;
+      box-shadow: 0 1px 3px rgba(0,0,0,.13);
+      margin-top: 50vh; /* poussé de la moitié de hauteur de viewport */
+      transform: translateY(-50%); /* tiré de la moitié de sa propre hauteur */
+      border-radius: <?php echo $settings->radiusbox; ?>px;
+      padding-top: 15px;
+    }
+
+    .form {
+      display: flex;
+      justify-content: center;
+    }
+    .previewContainer {
+      width: 90%;
+      height: 30%;
+      background-image: url('<?php echo $settings->imageurl; ?>');
+      background-size: <?php echo $settings->imagesize; ?>;
+      <?php if($settings->effetvignettage == "Active"){ echo "box-shadow: inset 0 0 100px black;";} ?>
+    }
+  </style>
+  <i style="color:white;margin:3px;">Ceci n'est qu'une représentation approximative de l'écran de login.</i>
+</div>
 </div>
 
 <div id="informations" class="tabs">
   <div class="version"><span>V <?php echo ACIVERSION; ?></span></div>
   <p class="infos">Admin Change Image à étais conçu dans le cadre d'un stage pour l'entreprise <a href="https://nereus-water.com" target="_blank">Nereus Water</a></p>
   <h2>Changelog :</h2>
+  <h3>V 0.2.5 : Preview Mode</h3>
+    <ul>
+      <li>Prévisualisation du logo</li>
+      <li>Prévisualisation de l'arrière plan</li>
+      <li>Ajout de l'onglet Preview qui permet d'avoir un aperçu de la page de login sans ce déconnecté</li>
+    </ul>
     <h3>V 0.2.1 : Correction de bug</h3>
-    <ul>
-      <li>Correction de bug sur le système d'onglet</li>
-    </ul>
+      <ul>
+        <li>Correction de bug sur le système d'onglet</li>
+      </ul>
     <h3>V 0.2.0 : Correction de bug & Implémentation d'options</h3>
-    <ul>
-      <li>Ajout d'un système d'onglet</li>
-      <li>Possibilité de redirigé le logo sur la page d'accueil</li>
-      <li>Possibilité d'ajouter un texte au survol du logo</li>
-      <li>Ajout d'un effet vignettage</li>
-      <li>Possibilité de modifier la couleur du texte</li>
-    </ul>
+      <ul>
+        <li>Ajout d'un système d'onglet</li>
+        <li>Possibilité de redirigé le logo sur la page d'accueil</li>
+        <li>Possibilité d'ajouter un texte au survol du logo</li>
+        <li>Ajout d'un effet vignettage</li>
+        <li>Possibilité de modifier la couleur du texte</li>
+      </ul>
     <h3>V 0.1.0 : Les bases sont posées</h3>
       <ul>
         <li>Possibilité de modifier le logo</li>
--- app.js
+++ app.js
@@ -10,12 +10,34 @@ $('.my-color-field').wpColorPicker(myOptions);
 
 $(document).ready(function(){
   $("#informations").hide();
+  $("#preview").hide();
   $("#option-click").click(function(){
     $("#options").show();
     $("#informations").hide();
+    $("#preview").hide();
+  });
+  $("#preview-click").click(function(){
+    $("#options").hide();
+    $("#informations").hide();
+    $("#preview").show();
   });
   $("#info-click").click(function(){
     $("#options").hide();
     $("#informations").show();
-  });
+    $("#preview").hide();
 });
+});
+
+$( "#text-url" )
+  .keyup(function() {
+    var value = $( this ).val();
+    $( "#imgOutput" ).attr( "src", value );
+  })
+  .keyup();
+
+  $( "#bg-url" )
+    .keyup(function() {
+      var value = $( this ).val();
+      $( "#bgOutput" ).attr( "src", value );
+    })
+    .keyup();
